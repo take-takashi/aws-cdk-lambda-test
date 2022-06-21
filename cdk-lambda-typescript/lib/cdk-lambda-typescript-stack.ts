e098795d001d2111c4b157c245aaf6c8df25b9ff
TAKE Takashi <nakamizo.takashi@gmail.com>
2022-06-21 09:46:18 +0000

    set stack

--- cdk-lambda-typescript/lib/cdk-lambda-typescript-stack.ts
+++ cdk-lambda-typescript/lib/cdk-lambda-typescript-stack.ts
@@ -1,16 +1,16 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { aws_lambda, aws_lambda_nodejs, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as path from 'path';
 
 export class CdkLambdaTypescriptStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkLambdaTypescriptQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const func = new aws_lambda_nodejs.NodejsFunction(this, 'testFunction', {
+      functionName: 'testFunction',
+      entry: path.join(__dirname, '../lambda/index.ts'),
+      runtime: aws_lambda.Runtime.NODEJS_16_X,
+      handler: 'handler',
+    })
   }
 }
